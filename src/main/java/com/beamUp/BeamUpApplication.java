9f95df6e71fc3f33a76c99eac0ba2afcb8a82c8b
iltertaha <ilteraktolga@gmail.com>
2022-05-15 23:20:10 +0300

    Add spring initializr files

---
+++ src/main/java/com/beamUp/BeamUpApplication.java
@@ -0,0 +1,13 @@
+package com.beamUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeamUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeamUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beamUp/BeamUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beamUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeamUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
